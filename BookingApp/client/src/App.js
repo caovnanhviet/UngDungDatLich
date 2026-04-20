9df40485c83748b0096b0f68d3ebed49bdbb74c6
caovanhviet@dtu.edu.vn <caovanhviet@dtu.edu.vn>
2026-04-20 19:08:57 +0700

    initial commit

--- BookingApp/client/src/App.js
+++ BookingApp/client/src/App.js
@@ -1,7 +1,13 @@
+import { useState } from "react";
+import Login from "./Login";
 import Booking from "./Booking";
 
-function App(){
-  return <Booking />;
+function App() {
+  const [token, setToken] = useState(localStorage.getItem("token"));
+
+  return token
+    ? <Booking setToken={setToken} /> 
+    : <Login setToken={setToken} />;
 }
 
 export default App;
